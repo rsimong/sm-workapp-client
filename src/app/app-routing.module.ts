9f1a25f838e840a5aaeb4c01a93412f7b4a9b561
Ricard Simon <rsimon@softwaremakers.es>
2021-01-15 20:48:07 +0100

    Define basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'app', loadChildren: () => import('@modules/app/app.module').then(m => m.AppModule) },
+  { path: '**', redirectTo: 'app' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
